43077afa8d226165fcdfa0ccdc9d11483cc3b76a
Amrito Ben Hasda <matheusjustinhasda@gmail.com>
2025-07-29 04:26:20 +0600

    added login required decorator for protect main page

--- core/views.py
+++ core/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login_view')
 def home_view(request):
     return render(request, 'pages/index.html')
 
